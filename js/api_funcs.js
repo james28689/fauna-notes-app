a757266c235f9475824ca7c865fa7dc1645c3caf
James Watling <james.watling@gmail.com>
2021-05-16 18:27:25 +0100

    Changed delete function fetch method to DELETE

--- js/api_funcs.js
+++ js/api_funcs.js
@@ -25,7 +25,7 @@ function openNote(noteID) {
 }
 
 function deleteNote(noteID) {
-    fetch("https://fauna-notes-api.herokuapp.com/deleteNote/" + noteID);
+    fetch("https://fauna-notes-api.herokuapp.com/deleteNote/" + noteID, { method: "DELETE" });
     $("#" + noteID).addClass("fadeout");
     setTimeout(function () {
         $("#" + noteID).remove();
